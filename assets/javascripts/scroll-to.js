58e772ea1bf5a2520aaf7543994cbaa99739dee9
francois-xavier <frxpoulin@gmail.com>
2014-05-19 15:25:32 -0400

    use of class instead of id as selector

--- assets/javascripts/scroll-to.js
+++ assets/javascripts/scroll-to.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#scroll-to').click(function () {
-        var scrollTopOffset = $('#section1').offset().top;
+        var scrollTopOffset = $('.services').offset().top;
         $('body').animate({scrollTop: scrollTopOffset}, 500, 'swing');
     });
 });

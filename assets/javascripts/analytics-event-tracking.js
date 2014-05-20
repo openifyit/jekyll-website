de115c2990ae08eec665d1d3750c783ad2465c9d
francois-xavier <frxpoulin@gmail.com>
2014-05-20 12:40:13 -0400

    simplify the event tracking

--- assets/javascripts/analytics-event-tracking.js
+++ assets/javascripts/analytics-event-tracking.js
@@ -1,21 +1,7 @@
 $(document).ready(function () {
-    $('#scroll-to').click(function () {
-       ga('send', 'event', 'button', 'click', 'scroll-arrow')
-    });
-
-    $('#learn-more-api').click(function () {
-        ga('send', 'event', 'button', 'click', 'learn-more-api')
-    });
-
-    $('#learn-more-realtime').click(function () {
-        ga('send', 'event', 'button', 'click', 'learn-more-realtime')
-    });
-
-    $('#learn-more-visualization').click(function () {
-        ga('send', 'event', 'button', 'click', 'learn-more-visualization')
-    });
-
-    $('.close').click(function(){
-        ga('send', 'event', 'button', 'click', 'close-modal');
+    $("[data-track='true']").each(function () {
+        $(this).click(function(event){
+            ga('send', 'event', 'button', 'click', event.target.id);
+        });
     });
 });

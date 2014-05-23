0d21f98aa4769746dfbe77cfdf42c33d1b7b9c30
Olivier Sylvain <g.oliviersylvain@gmail.com>
2014-05-23 10:39:49 -0400

    Added smooth scroll

--- assets/javascripts/scroll-to.js
+++ assets/javascripts/scroll-to.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
     $('#scroll-arrow').click(function () {
-        var scrollTopOffset = $('nav').offset().top;
-        if(!scrollTopOffset) {
-            scrollTopOffset = $('.services').offset().top;
-        }
-        $('body').animate({scrollTop: scrollTopOffset}, 500, 'swing');
+        var headerBottomOffset = $('header').height();
+        $('body').animate({scrollTop: headerBottomOffset}, 500, 'swing');
     });
 });
+
+function scrollTo(event, anchorElement) {
+    event.preventDefault();
+    var scrollTopOffset = $(anchorElement).offset().top;
+    $('body').animate({scrollTop: scrollTopOffset}, 500, 'swing');
+    if(history && history.pushState()) {
+        history.pushState("", null, anchorElement);
+    }
+}
